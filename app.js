3b7c844de1c4d5cd92bf55671e16191ba8d17b20
barrionuevoJ <julianbarrionuevo10@gmail.com>
2023-02-03 14:28:27 -0300

    Creación de home con estilos

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.get('/login', (req, res) => res.sendFile(path.resolve('./views/login.html'))
 
 app.get('/register', (req, res) => res.sendFile(path.resolve('./views/register.html')))
 
-app.get('/productCard', (req, res) => res.sendFile(path.resolve('./views/productCard.html')))
+app.get('/productCart', (req, res) => res.sendFile(path.resolve('./views/productCart.html')))
 
 app.get('/productDetail', (req, res) => res.sendFile(path.resolve('./views/productDetail.html')))
 
